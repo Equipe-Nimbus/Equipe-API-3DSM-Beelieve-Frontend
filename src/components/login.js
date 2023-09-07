52a30156c4e503f1ca292bc3dc612baa638a1763
HenningerJv <josevictor.henninger@gmail.com>
2023-09-07 19:33:41 -0300

    adicionando o componente da tela de login

---
+++ src/components/login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import api from '../services/api';
+import logo from '../images/logo.png'
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+    let navegate = useNavigate()
+
+    return (
+        <>
+            <div className='login'>
+                <img src={logo} alt="Sabiá Laranjeira" className='sabia' />
+                <div className='login-center'>
+                    <div className='mainContent'>
+                        <div className='state'>
+                            <form>
+                                <br></br>
+                                <p></p>
+                                <div className='logocentro'>
+                                    <img className="logo" src={logo} alt="logo" /></div>
+                                <br></br>
+                                <h3 className='login-text'>Login:</h3>
+                                <input type="text" required />
+                                <h3 className='login-text'>Senha:</h3>
+                                <input type="password" required />
+                                <p></p>
+                                <input type="submit" className="login_button" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Login
