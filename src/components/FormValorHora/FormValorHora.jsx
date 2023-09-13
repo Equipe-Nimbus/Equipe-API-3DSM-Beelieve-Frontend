bcfe2175825136f572c8892b474a2863d217f6f0
unknown <renannovo99@gmail.com>
2023-09-13 10:15:50 -0300

    começando a tabela

---
+++ src/components/FormValorHora/FormValorHora.jsx
@@ -0,0 +1,16 @@
+function FormValorHora() {
+
+    return(
+        <table>
+            <tr>
+                <th>Nível</th>
+                <th>Descrição</th>
+                <th>Orçamento</th>
+                <th>HH</th>
+                <th>Atribuição</th>
+            </tr>
+        </table>
+    );
+}
+
+export default FormValorHora;
