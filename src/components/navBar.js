e463f6508e4090803775a2f542be7b8ce9886576
HenningerJv <josevictor.henninger@gmail.com>
2023-09-13 11:07:26 -0300

    adicionando a NavBar com os itens aparecendo

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,26 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
-import logo from '../images/logo.png'
+
+import recurso from '../images/recurso.png'
+import relatorio from '../images/relatorio.png'
 import projeto from '../images/projeto.png'
 import usuario from '../images/usuario.png'
-import api from '../services/api';
+import logout from '../images/logout.png'
 
 function NavBar() {
+    const [logado, setLogado] = useState(Boolean)
+    const [nome, setNome] = useState('')
+    const [funcao, setFuncao] = useState('')
     const navigate = useNavigate()
-
-        return (
-            <><nav className="">
-                <Link className="" to={'/'}><img className="" src={logo} alt="" /></Link>
-                <div className="">
-                    <img className="" src={projeto} alt="" /> <Link className="" to={'/'}>Projetos</Link>
-                </div>
-                <div className="">
-                    <img className="" src={usuario} alt="" /> <h1 className="nomeUsuario">{`Nome: ${sessionStorage.getItem('nomeUsuario')}`}</h1>
-                    <span className="funcaoUsuario">{`${sessionStorage.getItem('funcaoUsuario')}`}</span>
-                </div>
-            </nav>
-            </>
-        )
+    return (
+        <><nav className="">
+            <Link className="" to={'/'}></Link>
+            <div className="">
+                <img className="" src={projeto} alt="" /> <a href=''>Projetos</a>
+                <img className="" src={recurso} alt="" /> <a href=''>Recursos</a>
+                <img className="" src={relatorio} alt="" /> <a href=''>Relatório</a>
+            </div>
+            <div className="">
+                <img className="" src={usuario} alt="" /> <h1 className="nomeUsuario">{`Nome: ${sessionStorage.getItem('nomeUsuario')}`}</h1>
+                <span className="funcaoUsuario">{`${sessionStorage.getItem('funcaoUsuario')}`}</span>
+            </div>
+            <img className="" src={logout} alt="" /> <button className='loggout'>Sair</button>
+        </nav>
+        </>
+    )
 }
 
 export default NavBar
