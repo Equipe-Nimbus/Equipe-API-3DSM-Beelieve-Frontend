cd658a400bb33b97579b03af6891ea756a3a2be1
HenningerJv <josevictor.henninger@gmail.com>
2023-09-17 18:05:08 -0300

    adicionando a estilizacao da navBar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 
+import style from '../style.css'
+import beelieveLogo from '../images/beelieveLogo.png'
 import recurso from '../images/recurso.png'
 import relatorio from '../images/relatorio.png'
 import projeto from '../images/projeto.png'
@@ -13,19 +15,38 @@ function NavBar() {
     const [funcao, setFuncao] = useState('')
     const navigate = useNavigate()
     return (
-        <><nav className="">
-            <Link className="" to={'/'}></Link>
-            <div className="">
-                <img className="" src={projeto} alt="" /> <a href=''>Projetos</a>
-                <img className="" src={recurso} alt="" /> <a href=''>Recursos</a>
-                <img className="" src={relatorio} alt="" /> <a href=''>Relatório</a>
-            </div>
-            <div className="">
-                <img className="" src={usuario} alt="" /> <h1 className="nomeUsuario">{`Nome: ${sessionStorage.getItem('nomeUsuario')}`}</h1>
-                <span className="funcaoUsuario">{`${sessionStorage.getItem('funcaoUsuario')}`}</span>
-            </div>
-            <img className="" src={logout} alt="" /> <button className='loggout'>Sair</button>
-        </nav>
+        <>
+            <nav className="navBar2">
+                <div className='logo'>
+                    <img className="fotoLogo" src={beelieveLogo} alt="logo" />
+                </div>
+                <div className="sideBar">
+                    <div className='navegadores'>
+                        <ul>
+                            <li>
+                                <a href='' className='navegador'><img className="fotoProjeto" src={projeto} alt="projetos" />Projetos</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <a href='' className='navegador'><img className="fotoRecurso" src={recurso} alt="" />Recurso</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <a href='' className='navegador'><img className="fotoRelatorio" src={relatorio} alt="" />Relatório</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='informacaoUsuario'>
+                        <img className="fotoUsuario" src={usuario} alt="" /> <h1 className="nomeUsuario">{`${sessionStorage.getItem('nomeUsuario')}`}</h1>
+                        <span className="funcaoUsuario">{`${sessionStorage.getItem('funcaoUsuario')}`}</span>
+                    </div>
+                </div>
+                <div className="loggoutButton">
+                    <button className='loggout'><img className="loggoutFoto" src={logout} alt="" />Sair</button>
+                </div>
+            </nav>
         </>
     )
 }
