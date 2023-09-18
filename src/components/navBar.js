779a5f8cdbe63fe762d27cb7fbd9c2a3601fae4a
HenningerJv <josevictor.henninger@gmail.com>
2023-09-18 07:34:04 -0300

    colocando a navBar modificada

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
-import logo from '../images/logo.png'
+
+import style from '../style.css'
+import beelieveLogo from '../images/beelieveLogo.png'
 import recurso from '../images/recurso.png'
 import relatorio from '../images/relatorio.png'
 import projeto from '../images/projeto.png'
@@ -12,49 +14,41 @@ function NavBar() {
     const [nome, setNome] = useState('')
     const [funcao, setFuncao] = useState('')
     const navigate = useNavigate()
-
-    async function veLogado() {
-        let dadoUsuario = await api.get('/confereLogado')
-        if (dadoUsuario.data.logado) {
-            setLogado(true)
-            setNome(dadoUsuario.data.nome)
-            setFuncao(dadoUsuario.data.funcao)
-            //console.log(funcao)
-        }
-        else {
-            setLogado(false)
-        }
-
-    }
-
-    async function loggout() {
-        const log = await api.get('/loggout')
-        console.log(log.data)
-        navigate('/')
-    }
-
-    useEffect(() => {
-        veLogado()
-    })
-
-    if (logado) {
-        return (
-            <><nav className="">
-                <Link className="" to={'/'}><img className="" src={logo} alt="" /></Link>
-                <div className="">
-                    <img className="" src={projeto} alt="" /> <Link className="" to={'/'}>Projetos</Link>
-                    <img className="" src={recurso} alt="" /> <Link className="" to={'/'}>Recursos</Link>
-                    <img className="" src={relatorio} alt="" /> <Link className="" to={'/'}>Relatórios</Link>
+    return (
+        <>
+            <nav className="navBar2">
+                <div className='logo'>
+                    <img className="fotoLogo" src={beelieveLogo} alt="logo" />
                 </div>
-                <div className="">
-                    <img className="" src={usuario} alt="" /> <h1 className="nomeUsuario">{`Nome: ${sessionStorage.getItem('nomeUsuario')}`}</h1>
-                    <span className="funcaoUsuario">{`${sessionStorage.getItem('funcaoUsuario')}`}</span>
+                <div className="sideBar">
+                    <div className='navegadores'>
+                        <ul>
+                            <li>
+                                <a href='' className='navegador'><img className="fotoProjeto" src={projeto} alt="projetos" />Projetos</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <a href='' className='navegador'><img className="fotoRecurso" src={recurso} alt="" />Recurso</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <a href='' className='navegador'><img className="fotoRelatorio" src={relatorio} alt="" />Relatório</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='informacaoUsuario'>
+                        <img className="fotoUsuario" src={usuario} alt="" /> <h1 className="nomeUsuario">{`${sessionStorage.getItem('nomeUsuario')}`}</h1>
+                        <span className="funcaoUsuario">{`${sessionStorage.getItem('funcaoUsuario')}`}</span>
+                    </div>
                 </div>
-                <img className="" src={logout} alt="" /> <button className='loggout' onClick={loggout}>Sair</button>
+                <div className="loggoutButton">
+                    <button className='loggout'><img className="loggoutFoto" src={logout} alt="" />Sair</button>
+                </div>
             </nav>
-            </>
-        )
-    }
+        </>
+    )
 }
 
 export default NavBar
