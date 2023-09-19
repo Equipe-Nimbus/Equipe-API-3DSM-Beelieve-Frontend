8e495e4e32954e8d87e091697d2e153b6ec54131
Renan-Neves <renannovo99@gmail.com>
2023-09-19 11:50:48 -0300

    atualizando a estilização e preparando para ler o json

--- src/components/FormValorHora/FormValorHora.jsx
+++ src/components/FormValorHora/FormValorHora.jsx
@@ -2,23 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function FormValorHora() {
-    const [data, setData] = useState([]);
 
-    useEffect(() => {
-      fetchData();
-    }, []);
-  
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(''); // entrar com a tabela
-        setData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
 
     return(
-        <table class="table-fixed text-on-light  mt-5 w-1/3 text-left border-collapse border-spacing-0.5 border">
+        <div class="bg-bg100 m-5 rounded-md p-7 drop-shadow-md">
+        <table class="table-fixed mt-5 w-2/3 text-center">
             <thead class="bg-primary98 text-base uppercase ">
                 <tr>
                     <th class="border">Nível</th>
@@ -30,23 +18,24 @@ function FormValorHora() {
             </thead>
             <tbody class="text-lg">
                 <tr>
-                    <td >a</td>
-                    <td>{}</td>
-                    <td>{}</td>
-                    <td>{}</td>
-                    <td>{}</td>
+                    <td class="border px-1 break-all">a</td>
+                    <td class="border px-1 break-all">{}</td>
+                    <td class="border px-1 break-all">{}</td>
+                    <td class="border px-1 break-all"><form><input type="text" id="hora" name="hora" /></form></td>
+                    <td class="border px-1 break-all">{}</td>
                 </tr>
             {/* {data.map((row) => (
                 <tr key={row}>
-                    <td class="border">{}</td>
-                    <td class="border">{}</td>
-                    <td class="border">{}</td>
-                    <td class="border"><form><input type="text" id="hora" name="hora" /></form></td>
-                    <td class="border">{}</td>
+                    <td class="border px-1 break-all">{}</td>
+                    <td class="border px-1 break-all">{}</td>
+                    <td class="border px-1 break-all">{}</td>
+                    <td class="border px-1 break-all"><form><input type="text" id="hora" name="hora" /></form></td>
+                    <td class="border px-1 break-all">{}</td>
                 </tr>
             ))} */}
             </tbody>
         </table>
+        </div>
     );
 }
 
