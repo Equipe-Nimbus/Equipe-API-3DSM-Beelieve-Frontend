71777d1d1f92fceacb8b573a0fc9678cfae1fe9d
HenningerJv <josevictor.henninger@gmail.com>
2023-09-19 08:21:08 -0300

    adicionando a estilizacao da tela de login

--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import api from '../services/api';
-import logo from '../images/logo.png'
-import { toast } from 'react-toastify';
+
+import loginStyle from '../loginStyle.css'
+import detalhe from '../images/detalhe.png'
+import beelieveLogo from '../images/beelieveLogo.png';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
@@ -10,23 +11,29 @@ function Login() {
 
     return (
         <>
-            <div className='login'>
-                <img src={logo} alt="Sabiá Laranjeira" className='sabia' />
-                <div className='login-center'>
+            <div className='login-body'>
+                <div className='login-box'>
                     <div className='mainContent'>
                         <div className='state'>
+                            <h1><img src={beelieveLogo} alt="beelieveLogo" className='loginLogo' />BeeLieve</h1>
+                            <img src={detalhe} className='beeHive' />
                             <form>
                                 <br></br>
                                 <p></p>
-                                <div className='logocentro'>
-                                    <img className="logo" src={logo} alt="logo" /></div>
                                 <br></br>
-                                <h3 className='login-text'>Login:</h3>
-                                <input type="text" required />
-                                <h3 className='login-text'>Senha:</h3>
-                                <input type="password" required />
+                                <div className='loginTitle'>
+                                    <h2>Bem Vindo</h2>
+                                </div>
+                                <div className='loginText'>
+                                    <h3 className='login-text'>Login:</h3>
+                                    <input type="text" className='loginInput' required />
+                                </div>
+                                <div className='senhaText'>
+                                    <h3 className='login-text'>Senha:</h3>
+                                    <input type="password" className='senhaInput' required />
+                                </div>
                                 <p></p>
-                                <input type="submit" className="login_button" value='Entrar' />
+                                <input type="submit" className="login_button" value='Entre' />
                             </form>
                         </div>
                     </div>
