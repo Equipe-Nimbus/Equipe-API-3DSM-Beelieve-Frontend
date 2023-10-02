13d2e1298a61651db4504f0b73d7b2927ee09d89
HenningerJv <josevictor.henninger@gmail.com>
2023-10-02 08:30:51 -0300

    adicionando partes restantes como info relevante

--- src/components/VisaoGeral.jsx
+++ src/components/VisaoGeral.jsx
@@ -38,6 +38,20 @@ function VisaoGeral({nomeProjeto, descricaoProjeto, liderProjeto}) {
         </span>
         <span>{liderProjeto}</span>
       </span>
+      <br />
+      <span className="mt-2 inline-grid grid-cols-2 gap-2 text-n20">
+        <span className="font-semibold text-complementary-20">
+          Info Relevante:
+        </span>
+        <span>{liderProjeto}</span>
+      </span>
+      <br />
+      <span className="mt-2 inline-grid grid-cols-2 gap-2 text-n20">
+        <span className="font-semibold text-complementary-20">
+          Info Relevante:
+        </span>
+        <span>{liderProjeto}</span>
+      </span>
     </div>
   )
 }
