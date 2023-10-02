150751e856de2f35c88cfb0e0a158031c06c49e6
Renan Neves <renannovo99@gmail.com>
2023-10-01 22:13:43 -0300

    feat: Filtro de dados do excel

--- src/components/LerExcel.jsx
+++ src/components/LerExcel.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { json } from 'react-router-dom';
 import * as XLSX from "xlsx";
  
 function LerExcel() {
@@ -11,41 +12,71 @@ function LerExcel() {
         reader.onload = (e) => {
             const data = e.target.result;
             const workbook = XLSX.read(data, {type: "binary"});
-            const sheetName = workbook.SheetNames[0];
+            const sheetName = workbook.SheetNames[1];
             const sheet = workbook.Sheets[sheetName];
-            const parsedData = XLSX.utils.sheet_to_json(sheet);
-            setdata(parsedData)
+            const parsedData = XLSX.utils.sheet_to_json(sheet, { header: 1 }); 
+            const selectedData = parsedData.map(row => [row[0]]);
+
+            setdata(selectedData);
         };
     }
- 
+
+    const jsonWBS = JSON.stringify(data.map(item => {
+        const value = item.toString();
+        const valueArray = value.split(' ');
+    
+        let result = {};
+    
+        if (valueArray.length === 1) {
+        } else {
+            result.usavel = value;
+        }
+        
+        return result;
+    }));
+    
+    const parsedJsonWBS = JSON.parse(jsonWBS);
+    const filteredWBS = parsedJsonWBS.filter(obj => Object.keys(obj).length !== 0);
+    filteredWBS.pop()
+      
+    const niveis = filteredWBS.reduce((acc, item, index) => {
+        const dots = (item.usavel ?? '').trim().split('.').length - 1;
+      
+        if (index === 0) {
+          acc.projeto = (item.usavel ?? '').trim();
+        } else if (dots === 1) {
+          if (!acc.subProjeto) {
+            acc.subProjeto = [];
+          }
+          acc.subProjeto.push((item.usavel ?? '').trim());
+        } else if (dots === 2) {
+          if (!acc.subNivel) {
+            acc.subNivel = [];
+          }
+          acc.subNivel.push((item.usavel ?? '').trim());
+        }
+      
+        return acc;
+      }, {});
+
+    console.log(niveis);
+
     return (
         <div>
             <input
             type="file"
             accept=".xlsx, .xls"
             onChange={handleFileUpload}
+            className="bg-primary50 text-on-primary mb-5  flex items-center gap-0.5 rounded-[10px] p-2 text-lg font-semibold"
             />
 
-            {data.length > 0 &&(
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            {Object.keys(data[0]).map((key) => (
-                                <th key={key}>{key}</th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((row, index) =>(
-                            <tr key={index}>
-                                {Object.values(row).map((value, index) => (
-                                    <td key={index}>{value}</td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
+<tbody>
+    {data.map((row, index) => (
+        <tr key={index}>
+            <td>{row[0]}</td>
+        </tr>
+    ))}
+</tbody>
         </div>
     )
 }    
--- src/pages/NovoProjeto.jsx
+++ src/pages/NovoProjeto.jsx
@@ -3,18 +3,20 @@ import Button from "../components/Button";
 import FormCadastroProjeto from "../components/FormCadastroProjeto/FormCadastroProjeto";
 
 import { HiOutlineArrowUpCircle } from "react-icons/hi2";
+import LerExcel from "../components/LerExcel";
 
 function NovoProjeto() {
   return (
     <>
       <div className="bg-bg100 m-5 rounded-md p-7 drop-shadow-md">
-        <Button
+        {/* <Button
           texto="Subir arquivo"
           tipo="button"
           iconeOpcional={HiOutlineArrowUpCircle}
           iconeTamanho="24px"
           className="bg-primary50 text-on-primary mb-5  flex items-center gap-0.5 rounded-[10px] p-2 text-lg font-semibold"
-        />
+        /> */}
+        <LerExcel/>
         <hr className="border-n90" />
         <FormCadastroProjeto />
       </div>
