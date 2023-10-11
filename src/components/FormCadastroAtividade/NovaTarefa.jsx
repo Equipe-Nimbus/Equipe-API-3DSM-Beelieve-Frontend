178bcc13881f7304a3f181d230917510dfd8d1fd
Vinicius <yinioliveira@gmail.com>
2023-10-11 00:39:11 -0300

    refactor: Refiz o formulário de tarefas utilizando o react-hook-form

--- src/components/FormCadastroAtividade/NovaTarefa.jsx
+++ src/components/FormCadastroAtividade/NovaTarefa.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { useLocation, useParams } from "react-router";
-import { AiOutlineArrowLeft } from 'react-icons/ai';
-import TabFormTarefas from "./TabFormTarefas";
+import React from "react"
+import { useLocation, useParams } from "react-router"
+import { AiOutlineArrowLeft } from "react-icons/ai"
+import TabFormTarefas from "./TabFormTarefas"
 
 function NovaTarefa() {
-  const {idTarefa} = useParams()
+  const { idTarefa } = useParams()
 
   const location = useLocation()
-  const {tipo_pai, subprojeto, nomeProjeto} = location.state
+  const { tipo_pai, subprojeto, nomeProjeto } = location.state
 
   let ordemSubProjeto = ""
   let nomeSubProjeto = ""
 
-  if(tipo_pai === 'subprojeto'){
+  if (tipo_pai === "subprojeto") {
     ordemSubProjeto = subprojeto.ordem_sub_projeto
     nomeSubProjeto = subprojeto.nome_sub_projeto
   } else {
@@ -22,22 +22,27 @@ function NovaTarefa() {
 
   return (
     <>
-    
-    <button 
+      <button
         onClick={() => window.history.back()}
-        className="flex items-center ml-10 text-gray-50 underline font-semibold"
-    >
+        className="text-gray-50 ml-10 flex items-center font-semibold underline"
+      >
         <AiOutlineArrowLeft className="mr-2" />
         Voltar
-    </button>
-      <div className="bg-bg100 m-5 rounded-md p-7 drop-shadow-md">
+      </button>
+      <div className="m-5 rounded-md bg-bg100 p-7 drop-shadow-md">
         <div>
-          <TabFormTarefas tarefas={subprojeto.tarefas} tipo_pai={tipo_pai} id={idTarefa} ordem={ordemSubProjeto} nomePacote={nomeSubProjeto} nomeProjeto={nomeProjeto}/>
+          <TabFormTarefas
+            listaTarefas={subprojeto.tarefas}
+            tipoPai={tipo_pai}
+            idPai={idTarefa}
+            ordem={ordemSubProjeto}
+            nomePacote={nomeSubProjeto}
+            nomeProjeto={nomeProjeto}
+          />
         </div>
       </div>
-      
     </>
-  );
+  )
 }
 
-export default NovaTarefa;
+export default NovaTarefa
--- src/components/FormCadastroAtividade/TabFormTarefas.jsx
+++ src/components/FormCadastroAtividade/TabFormTarefas.jsx
@@ -1,42 +1,65 @@
 import React, { useEffect, useState } from "react"
+import { useForm, useFieldArray } from "react-hook-form"
+
 import { BiTrash } from "react-icons/bi"
-import {AiOutlinePlus} from "react-icons/ai"
+import { AiOutlinePlus } from "react-icons/ai"
 //import schemaInsercaoAtividade from './validationAtividade';
 import Button from "../Button"
 import axios from "../../services/axios"
 
-const TabFormTarefas = ({ tarefas, tipo_pai, id, ordem, nomePacote, nomeProjeto }) => {
-  const [tarefa, setTarefa] = useState([
+const TabFormTarefas = ({
+  listaTarefas,
+  tipoPai,
+  idPai,
+  ordem,
+  nomePacote,
+  nomeProjeto,
+}) => {
+
+  const [tarefas, setTarefas] = useState([
     {
       id: "",
       descricao: "",
       resultadoEsperado: "",
-      status: "",
+      status: 0,
       peso: "",
       prazo: "",
     },
   ])
 
+  const { register, control, handleSubmit, setValue } = useForm({
+    defaultValues: {
+      tarefas: tarefas
+    },
+  })
+
+  const { fields } = useFieldArray({
+    control,
+    keyName: "customId",
+    name: "tarefas",
+  })
+
   useEffect(() => {
-    if (tarefas.length > 0) {
-      const novasTarefas = tarefas.map((atividade) => {
+    if (listaTarefas.length > 0) {
+      const novasTarefas = listaTarefas.map((atividade) => {
         const novaTarefa = {
           id: atividade.id_tarefa,
           descricao: atividade.descricao_atividade_tarefa,
           resultadoEsperado: atividade.resultado_esperado_tarefa,
-          status: atividade.status_tarefa,
+          status: atividade.status_tarefa ? atividade.status_tarefa : 0,
           peso: atividade.peso_tarefa,
           prazo: atividade.prazo_tarefa,
         }
         return novaTarefa
       })
 
-      setTarefa(novasTarefas)
+      setTarefas(novasTarefas)
+      setValue("tarefas", tarefas)
     }
   }, [])
 
   const addRow = () => {
-    const novaTarefa = [...tarefa]
+    const novaTarefa = [...tarefas]
     const novaLinha = {
       id: "",
       descricao: "",
@@ -47,49 +70,49 @@ const TabFormTarefas = ({ tarefas, tipo_pai, id, ordem, nomePacote, nomeProjeto 
     }
 
     novaTarefa.push(novaLinha)
-    setTarefa(novaTarefa)
+    setTarefas(novaTarefa)
   }
 
-  const handleDescricao = (evento, index) => {
-    const novaDescricao = evento.target.value
-    const novaTabela = [...tarefa]
-    novaTabela[index].descricao = novaDescricao
-    setTarefa(novaTabela)
+  const deleteRow = async (index) => {
+    const tarefasExistentes = [...tarefas]
+    tarefasExistentes.splice(index, 1)
+
+    setTarefas(tarefasExistentes)
   }
 
-  const handleResultadoEsperado = (evento, index) => {
-    const novoResultadoEsperado = evento.target.value
-    const novaTabela = [...tarefa]
-    novaTabela[index].resultadoEsperado = novoResultadoEsperado
-    setTarefa(novaTabela)
+  useEffect(() => {
+    setValue("tarefas", tarefas)
+  }, [tarefas])
+
+  function gerarJsonTarefas(tarefas) {
+    const listaTarefas = {
+      tipo_pai: tipoPai,
+      id_pai: idPai,
+      lista_tarefas: [],
+    }
+
+    tarefas.forEach((atividade) => {
+      listaTarefas.lista_tarefas.push({
+        id_tarefa: atividade.id,
+        descricao_atividade_tarefa: atividade.descricao,
+        resultado_esperado_tarefa: atividade.resultadoEsperado,
+        peso_tarefa: parseInt(atividade.peso),
+        status_tarefa: atividade.status,
+        prazo_tarefa: atividade.prazo,
+      })
+    })
+
+    return listaTarefas
   }
 
-  const handlePeso = (evento, index) => {
-    const novoPeso = evento.target.value
-    const novaTabela = [...tarefa]
-    novaTabela[index].peso = novoPeso
-    setTarefa(novaTabela)
-  }
+  const saveTarefa = async (data) => {
+    console.log(data.tarefas)
 
-  const handlePrazo = (evento, index) => {
-    const novoPrazo = evento.target.value
-    const novaTabela = [...tarefa]
-    novaTabela[index].prazo = novoPrazo
-    setTarefa(novaTabela)
-  }
+    const listaTarefasPreenchidas = gerarJsonTarefas(data.tarefas)
+    console.log(listaTarefasPreenchidas)
 
-  const handleStatus = (evento, index) => {
-    const novoStatus = evento.target.checked ? 1 : 0
-    const novaTabela = [...tarefa]
-    novaTabela[index].status = novoStatus
-    setTarefa(novaTabela)
-  }
-
-  const saveTarefa = async () => {
-    const geraJsonTarefas = listaTarefas()
-
-    await axios
-      .put("/tarefa/atualizar", geraJsonTarefas)
+    /* await axios
+      .put("/tarefa/atualizar", listaTarefasPreenchidas)
       .then((response) => {
         if (response.status === 200) {
           window.alert("Tarefas atualizadas com sucesso!")
@@ -101,144 +124,105 @@ const TabFormTarefas = ({ tarefas, tipo_pai, id, ordem, nomePacote, nomeProjeto 
         } else {
           console.error("Erro:", error)
         }
-      })
-
-    /*schemaInsercaoAtividade.validate( tarefa )
-            .then( tarefa => {
-                axios.post( '/tarefa/cadastrar', tarefa ).then( response => setTarefa( response.tarefa ));
-            } )
-            .catch( errors => {
-                console.error( errors );
-            } );*/
-
-    //console.log("Dados Salvos:", geraJsonTarefas)
-  }
-
-  const deleteRow = async (index) => {
-    const tarefasExistentes = [...tarefa]
-    tarefasExistentes.splice(index, 1)
-
-    setTarefa(tarefasExistentes)
-  }
-
-  function listaTarefas() {
-    const listaTarefas = {
-      tipo_pai: tipo_pai,
-      id_pai: id,
-      lista_tarefas: [],
-    }
-
-    tarefa.forEach((atividade) => {
-      listaTarefas.lista_tarefas.push({
-        id_tarefa: atividade.id,
-        descricao_atividade_tarefa: atividade.descricao,
-        resultado_esperado_tarefa: atividade.resultadoEsperado,
-        peso_tarefa: atividade.peso,
-        status_tarefa: atividade.status,
-        prazo_tarefa: atividade.prazo,
-      })
-    })
-
-    return listaTarefas
+      }) */
   }
 
   return (
     <div>
-      <h1 className="text-complementary-20 text-3xl mb-5">{nomeProjeto}</h1>
-      <h2 className="text-2xl mb-2">Pacote de trabalho: {`${ordem} - ${nomePacote}`}</h2>
+      <h1 className="mb-5 text-3xl text-complementary-20">{nomeProjeto}</h1>
+      <h2 className="mb-2 text-2xl">
+        Pacote de trabalho: {`${ordem} - ${nomePacote}`}
+      </h2>
       <hr className="border-n90" />
 
       <div className="flex flex-col gap-2">
         <div className="ps-40">
           <button
             onClick={addRow}
-            className="flex mt-9 place-self-end rounded-[10px] bg-primary50 p-1 text-lg font-semibold text-on-primary ml-4"
+            className="ml-4 mt-9 flex place-self-end rounded-[10px] bg-primary50 p-1 text-lg font-semibold text-on-primary"
           >
             Adicionar tarefa
             <AiOutlinePlus className="ml-1 mt-2" />
           </button>
         </div>
 
-        <table class="mx-auto mt-5 w-4/5">
-          <thead className="bg-primary98 p-10 text-base uppercase">
-            <tr>
-              <th class="px-6 py-3">Tarefa</th>
-              <th class="">Descrição</th>
-              <th class="">Resultado Esperado</th>
-              <th class="">Execução</th>
-              <th class="">Peso</th>
-              <th class="">Previsão</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tarefa.map((item, index) => (
-              <tr key={index} className="border-b border-n90">
-                <td class="px-4 py-1.5 text-center text-lg font-semibold">
-                  {index + 1}
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    className="w-full"
-                    value={tarefa[index].descricao}
-                    onChange={(e) => handleDescricao(e, index)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    className="w-full"
-                    value={tarefa[index].resultadoEsperado}
-                    onChange={(e) => handleResultadoEsperado(e, index)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="checkbox"
-                    checked={tarefa[index].status === 1}
-                    className="w-full"
-                    value={tarefa[index].status}
-                    onChange={(e) => handleStatus(e, index)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="number"
-                    className="w-full"
-                    value={tarefa[index].peso}
-                    onChange={(e) => handlePeso(e, index)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="date"
-                    className="w-full"
-                    value={tarefa[index].prazo}
-                    onChange={(e) => handlePrazo(e, index)}
-                  />
-                </td>
-                <td className="text-center">
-                  <button onClick={() => deleteRow(index)}>
-                    <BiTrash
-                      color="#000000"
-                      size={24}
-                      className="hover:fill-primary50"
-                    />
-                  </button>
-                </td>
+        <form onSubmit={handleSubmit(saveTarefa)}>
+          <table class="mx-auto mt-5 w-4/5">
+            <thead className="bg-primary98 p-10 text-base uppercase">
+              <tr>
+                <th class="px-6 py-3">Tarefa</th>
+                <th class="">Descrição</th>
+                <th class="">Resultado Esperado</th>
+                <th class="">Execução</th>
+                <th class="">Peso</th>
+                <th class="">Previsão</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {fields.map((tarefa, index) => (
+                <tr key={index} className="border-b border-n90">
+                  <td class="px-4 py-1.5 text-center text-lg font-semibold">
+                    {index + 1}
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      {...register(`tarefas[${index}].descricao`)}
+                      defaultValue={tarefa.descricao}
+                      className="w-full"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      {...register(`tarefas[${index}].resultadoEsperado`)}
+                      className="w-full"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      {...register(`tarefas[${index}].status`)}
+                      checked={fields[index].status === 1}
+                      className="w-full"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      {...register(`tarefas[${index}].peso`)}
+                      className="w-full"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="date"
+                      {...register(`tarefas[${index}].prazo`)}
+                      className="w-full"
+                    />
+                  </td>
+                  <td className="text-center">
+                    <button onClick={() => deleteRow(index)}>
+                      <BiTrash
+                        color="#000000"
+                        size={24}
+                        className="hover:fill-primary50"
+                      />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-        <div className="place-self-end mr-40">
-          <Button
-            texto="Salvar"
-            tipo="button"
-            onClick={saveTarefa}
-            className="rounded-[10px] bg-primary50 p-2 text-lg font-semibold text-on-primary"
-          />
-        </div>
+          <div className="mr-40 place-self-end">
+            <Button
+              texto="Salvar"
+              tipo="submit"
+              className="rounded-[10px] bg-primary50 p-2 text-lg font-semibold text-on-primary"
+            />
+          </div>
+        </form>
       </div>
     </div>
   )
