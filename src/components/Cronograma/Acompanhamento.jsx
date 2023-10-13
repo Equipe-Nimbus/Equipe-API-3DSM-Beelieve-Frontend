38e5812985db2f57df4b254376108c6aa6c6fc4e
Vinicius <yinioliveira@gmail.com>
2023-10-13 16:16:04 -0300

    fix: campos do progresso real não são editáveis

--- src/components/Cronograma/Acompanhamento.jsx
+++ src/components/Cronograma/Acompanhamento.jsx
@@ -65,6 +65,7 @@ function Acompanhamento({ idProjeto }) {
                 `cronograma[${indexMes}].niveis[${indexNivel}].progresso_real`,
               )}
               className="text-center"
+              disabled
             />
           </td>
         ))}
