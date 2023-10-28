7ec7d312bd38550361ca08ec2d1f544d43753e74
HenningerJv <josevictor.henninger@gmail.com>
2023-10-28 13:35:01 -0300

    adicionando a estilizacao da tela de alteracao de dados do usuario

--- src/components/FormAlterarDadosUsuario/FormAlterarDadosUsuario.jsx
+++ src/components/FormAlterarDadosUsuario/FormAlterarDadosUsuario.jsx
@@ -113,126 +113,154 @@ function AlterarUsuario() {
 
     return (
         <div>
-            <Button
-                tipo="submit"
-                texto="Excluir usuário"
-                iconeOpcional={BsPlayFill}
-                iconeTamanho="20px"
-                onClick={handleExcluirUsuarioClick}
-                className="mr-5 flex h-2/6 items-center gap-1 rounded-[10px] bg-primary51 p-2 text-lg font-semibold text-on-primary"
-            />
-            <hr className="border-n90" />
-            <form onSubmit={handleSubmit(alterarUsuario)}>
-                <input type="hidden" id="idUsuario" defaultValue={usuario.id_usuario} />
-                <h1 className="tex-center font-black">Alteração de Dados Usuario</h1>
-                <div className="mt-4 flex flex-col">
-                    <label
-                        htmlFor="nomeUsuario"
-                        className="text-base font-medium text-on-light"
-                    >
-                        Nome Completo:
-                    </label>
-                    <input
-                        type="text"
-                        className="w-1/2 rounded-md border border-n70 p-1"
-                        id="nomeUsuario"
-                        defaultValue={usuario.nome}
-                        {...register(`nomeUsuario`)}
-                    />
-                    {errors?.nomeUsuario && (
+            <div className="bg-bg100 m-5 rounded-md p-7 drop-shadow-md">
+                <Button
+                    tipo="submit"
+                    texto="Excluir usuário"
+                    iconeOpcional={BsPlayFill}
+                    iconeTamanho="20px"
+                    onClick={handleExcluirUsuarioClick}
+                    className="mr-5 mb-3 flex h-2/6 items-center gap-1 rounded-[10px] bg-primary51 p-2 text-lg font-semibold text-on-primary"
+                />
+                <hr className="border-n90" />
+                <form onSubmit={handleSubmit(alterarUsuario)}>
+                    <input type="hidden" id="idUsuario" defaultValue={usuario.id_usuario} />
+                    <div className="mt-4 flex flex-col">
+                        <h1 className="font-semibold text-2xl text-center">Alteração de Dados Usuario</h1>
+                    </div>
+
+                    <div className="mt-5 flex flex-col">
                         <label
                             htmlFor="nomeUsuario"
-                            className="text-sm font-light text-error"
+                            className="text-base font-medium text-on-light"
                         >
-                            {errors.nomePUsuario.message}
+                            Nome Completo:
                         </label>
-                    )}
-                </div>
-                <div className="mt-4 flex flex-col">
-                    <label
-                        htmlFor="emailUsuario"
-                        className="text-base font-medium text-on-light"
-                    >
-                        Email:
-                    </label>
-                    <input
-                        type="text"
-                        className="w-1/2 rounded-md border border-n70 p-1"
-                        defaultValue={usuario.email}
-                        {...register("emailUsuario")}
-                    />
-                    {errors?.usuarioEmail && (
+                        <input
+                            type="text"
+                            className="w-1/2 rounded-md border border-n70 p-1"
+                            id="nomeUsuario"
+                            defaultValue={usuario.nome}
+                            {...register(`nomeUsuario`)}
+                        />
+                        {errors?.nomeUsuario && (
+                            <label
+                                htmlFor="nomeUsuario"
+                                className="text-sm font-light text-error"
+                            >
+                                {errors.nomePUsuario.message}
+                            </label>
+                        )}
+                    </div>
+                    <div className="mt-4 flex flex-col">
                         <label
                             htmlFor="emailUsuario"
-                            className="text-sm font-light text-error"
+                            className="text-base font-medium text-on-light"
                         >
-                            {errors.usuarioEmail.message}
+                            Email:
                         </label>
-                    )}
-                </div>
-                <div className="mt-4 flex flex-col">
-                    <label
-                        htmlFor="senhaUsuario"
-                        className="text-base font-medium text-on-light"
-                    >
-                        Senha:
-                    </label>
-                    <input
-                        type="text"
-                        className="w-1/2 rounded-md border border-n70 p-1"
-                        defaultValue={usuario.senha}
-                        {...register("senhaUsuario")}
-                    />
-                    {errors?.usuarioSenha && (
+                        <input
+                            type="text"
+                            className="w-1/2 rounded-md border border-n70 p-1"
+                            defaultValue={usuario.email}
+                            {...register("emailUsuario")}
+                        />
+                        {errors?.usuarioEmail && (
+                            <label
+                                htmlFor="emailUsuario"
+                                className="text-sm font-light text-error"
+                            >
+                                {errors.usuarioEmail.message}
+                            </label>
+                        )}
+                    </div>
+                    <div className="mt-4 flex flex-col">
                         <label
                             htmlFor="senhaUsuario"
-                            className="text-sm font-light text-error"
+                            className="text-base font-medium text-on-light"
                         >
-                            {errors.usuarioSenha.message}
+                            Senha:
                         </label>
-                    )}
-                </div>
-                <div className="mt-4 flex flex-col">
-                    <label
-                        htmlFor="telefoneUsuario"
-                        className="text-base font-medium text-on-light"
-                    >
-                        Telefone:
-                    </label>
-                    <input
-                        type="number"
-                        className="w-1/2 rounded-md border border-n70 p-1"
-                        defaultValue={usuario.telefone}
-                        {...register("telefoneUsuario")}
-                    />
-                    {errors?.usuarioTelefone && (
+                        <input
+                            type="text"
+                            className="w-1/2 rounded-md border border-n70 p-1"
+                            defaultValue={usuario.senha}
+                            {...register("senhaUsuario")}
+                        />
+                        {errors?.usuarioSenha && (
+                            <label
+                                htmlFor="senhaUsuario"
+                                className="text-sm font-light text-error"
+                            >
+                                {errors.usuarioSenha.message}
+                            </label>
+                        )}
+                    </div>
+                    <div className="mt-4 flex flex-col">
+                        <label
+                            htmlFor="confirmarSenhaUsuario"
+                            className="text-base font-medium text-on-light">
+                            Confirme a Senha:
+                        </label>
+                        <input
+                            type="password"
+                            className="w-1/2 rounded-md border border-n70 p-1"
+                            {...register("confirmarSenhaUsuario")}
+                        />
+                        {errors?.usuarioSenha && (
+                            <label
+                                htmlFor="senhaConfirmeUsuario"
+                                className="text-sm font-light text-error"
+                            >
+                                {errors.usuarioConfirmeSenha.message}
+                            </label>
+                        )}
+                    </div>
+                    <div className="mt-4 flex flex-col">
                         <label
                             htmlFor="telefoneUsuario"
-                            className="text-sm font-light text-error"
+                            className="text-base font-medium text-on-light"
                         >
-                            {errors.usuarioTelefone.message}
+                            Telefone:
                         </label>
-                    )}
-                </div>
-                <div className="mt-4 flex flex-col">
-                    <label
-                        htmlFor="cargoUsuario"
-                        className="text-base font-medium text-on-light"
-                    >
-                        Cargo:
-                    </label>
-                    <select className="" name="cargoUsuario" required
-                        defaultValue={usuario.cargo}>
-                        <option value="EngenheiroChefe">Engenheiro Chefe</option>
-                        <option value="LiderProjeto">Líder de Projeto</option>
-                    </select>
-                </div>
-                <Button
-                    texto="Salvar"
-                    tipo="submit"
-                    className="rounded-[10px] bg-primary50 p-2 text-lg font-semibold text-on-primary"
-                />
-            </form>
+                        <input
+                            type="number"
+                            className="w-1/2 rounded-md border border-n70 p-1"
+                            defaultValue={usuario.telefone}
+                            {...register("telefoneUsuario")}
+                        />
+                        {errors?.usuarioTelefone && (
+                            <label
+                                htmlFor="telefoneUsuario"
+                                className="text-sm font-light text-error"
+                            >
+                                {errors.usuarioTelefone.message}
+                            </label>
+                        )}
+                    </div>
+                    <div className="mt-4 flex flex-col">
+                        <label
+                            htmlFor="cargoUsuario"
+                            className="text-base font-medium text-on-light"
+                        >
+                            Cargo:
+                        </label>
+                        <select className="w-1/2 border rounded border-n70 p-1" name="cargoUsuario" required
+                            defaultValue={usuario.cargo}>
+                            <option className="bg-primary98" value="EngenheiroChefe">Engenheiro Chefe</option>
+                            <option value="LiderProjeto">Líder de Projeto</option>
+                        </select>
+                    </div>
+
+                    <div className="mt-5 flex justify-end">
+                    <Button
+                        texto="Salvar"
+                        tipo="submit"
+                        className="rounded-[10px] bg-primary50 p-2 text-lg font-semibold text-on-primary mt-10"
+                    />
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
