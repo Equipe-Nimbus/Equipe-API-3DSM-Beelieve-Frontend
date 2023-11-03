12deba9395b6f7fb46b57fe97f00e6fc33391df5
Renan Neves <renannovo99@gmail.com>
2023-11-03 18:09:23 -0300

    feat: finalização da filtragem dos dados para a criação do excel

--- src/components/CriarExcel.jsx
+++ src/components/CriarExcel.jsx
@@ -3,27 +3,33 @@ import * as XLSX from "xlsx";
 
 function CriarExcel({projeto}){
 
-  console.log(projeto)
+
 
   const excelData = projeto && projeto.sub_projetos ? 
   [
       {   
           "Ordem Projeto": projeto.ordem_projeto,
-          "Projeto": projeto.nome_projeto 
+          "Projeto": projeto.nome_projeto, 
+          "Progresso": projeto.progresso_projeto + "%"
+
       }
   ].concat(
       projeto.sub_projetos.map(sub_projeto => ({
           "Ordem Projeto": sub_projeto.ordem_sub_projeto,
           "Projeto": sub_projeto.nome_sub_projeto,
+          "Progresso": sub_projeto.progresso_sub_projeto + "%"
       }))
   ).concat(
       projeto.sub_projetos.flatMap(sub_projeto => 
           sub_projeto.nivel_sub_projeto.map(nivel_sub_projeto => ({
               "Ordem Projeto": nivel_sub_projeto.ordem_nivel_sub_projeto,
               "Projeto": nivel_sub_projeto.nome_nivel_sub_projeto,
+              "Progresso": nivel_sub_projeto.progresso_nivel_sub_projeto + "%"
           }))
       )
   ) : [];
+
+  console.log(projeto)
 
   const compareOrdemProjeto = (a, b) => {
       const orderA = a["Ordem Projeto"].replace(/\.+$/, '');
@@ -61,6 +67,7 @@ if (projeto && projeto.sub_projetos) {
   });
 }
 
+tarefasData.sort(compareOrdemProjeto)
 
 const gerarExcel = (wbsData, tarefasData) => {
   const workbook = XLSX.utils.book_new();
@@ -75,7 +82,9 @@ const gerarExcel = (wbsData, tarefasData) => {
 }
 
 return (
-  <button onClick={() => gerarExcel(excelData, tarefasData)}>Download Excel</button>
+  <button onClick={() => gerarExcel(excelData, tarefasData)}
+  className="mr-5 flex h-2/6 items-center gap-1 rounded-[10px] bg-primary50 p-2 text-lg font-semibold text-on-primary">
+    Download Excel</button>
 );
 }
 
