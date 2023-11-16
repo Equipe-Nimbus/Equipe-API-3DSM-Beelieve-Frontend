77b2d28d76c384c41faa83f85c144ac6ce1d8bd1
HenningerJv <josevictor.henninger@gmail.com>
2023-11-16 09:12:06 -0300

    adicionando alteracoes na navbar conforme as funcoes dos usuarios

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -8,11 +8,11 @@ import { LiaUserTieSolid } from "react-icons/lia"
 import { BiLogOut } from "react-icons/bi"
 
 function NavBar() {
-  return (
-    <>
+  if (cargoUsuario === 'Gerente') {
+    return (
       <aside className="sticky top-0 flex h-screen min-w-max flex-col gap-3 bg-bg15 p-2">
         <div className="rounded bg-bg22 p-8">
-          <Link to="/projetos"> 
+          <Link to="/projetos">
             {<img
               alt="Logo da Beelieve"
               src={Beelieve}
@@ -50,9 +50,9 @@ function NavBar() {
             </div>
             <div className="flex h-[50px] flex-col">
               <p className="text-base font-semibold text-on-bg22">
-                Nome do usuário
+                {data.nomeUsuario}
               </p>
-              <p className="font-regular text-sm text-on-bg22">Cargo</p>
+              <p className="font-regular text-sm text-on-bg22">Gerente</p>
             </div>
           </div>
         </div>
@@ -61,8 +61,179 @@ function NavBar() {
           <p>Sair</p>
         </div>
       </aside>
-    </>
-  )
+    )
+  }
+  if (cargoUsuario === 'EngenheiroChefe') {
+    return (
+      <aside className="sticky top-0 flex h-screen min-w-max flex-col gap-3 bg-bg15 p-2">
+        <div className="rounded bg-bg22 p-8">
+          <Link to="/projetos">
+            {<img
+              alt="Logo da Beelieve"
+              src={Beelieve}
+              className="cursor-pointer"
+              width="180px"
+            />}
+          </Link>
+        </div>
+        <div className="flex h-full flex-col rounded bg-bg22 px-5">
+          <div className="h-full">
+            <ul className="my-10 flex flex-col gap-3">
+              <li className="flex cursor-pointer flex-row items-center gap-1 p-1.5 text-xl font-medium text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22">
+                <PiProjectorScreenChart
+                  color="#DADDE6"
+                  size={24}
+                  className="hover:fill-primary50"
+                />
+                <Link to="/projetos">Projetos</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="mb-5 flex gap-2">
+            <div className="flex h-[50px] flex-col">
+              <p className="text-base font-semibold text-on-bg22">
+                {data.nomeUsuario}
+              </p>
+              <p className="font-regular text-sm text-on-bg22">Engenheiro Chefe</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex cursor-pointer items-center gap-2 rounded bg-bg22 p-3 text-lg font-semibold text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22">
+          <BiLogOut color="#DADDE6" size={24} />
+          <p>Sair</p>
+        </div>
+      </aside>
+    )
+  }
+
+  if (cargoUsuario === 'EngenheiroChefe') {
+    return (
+      <aside className="sticky top-0 flex h-screen min-w-max flex-col gap-3 bg-bg15 p-2">
+        <div className="rounded bg-bg22 p-8">
+          <Link to="/projetos">
+            {<img
+              alt="Logo da Beelieve"
+              src={Beelieve}
+              className="cursor-pointer"
+              width="180px"
+            />}
+          </Link>
+        </div>
+        <div className="flex h-full flex-col rounded bg-bg22 px-5">
+          <div className="h-full">
+            <ul className="my-10 flex flex-col gap-3">
+              <li className="flex cursor-pointer flex-row items-center gap-1 p-1.5 text-xl font-medium text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22">
+                <PiProjectorScreenChart
+                  color="#DADDE6"
+                  size={24}
+                  className="hover:fill-primary50"
+                />
+                <Link to="/projetos">Projetos</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="mb-5 flex gap-2">
+            <div className="flex h-[50px] flex-col">
+              <p className="text-base font-semibold text-on-bg22">
+                {data.nomeUsuario}
+              </p>
+              <p className="font-regular text-sm text-on-bg22">Engenheiro Chefe</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex cursor-pointer items-center gap-2 rounded bg-bg22 p-3 text-lg font-semibold text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22">
+          <BiLogOut color="#DADDE6" size={24} />
+          <p>Sair</p>
+        </div>
+      </aside>
+    )
+  }
+
+  if (cargoUsuario === 'LiderPacote') {
+    return (
+      <aside className="sticky top-0 flex h-screen min-w-max flex-col gap-3 bg-bg15 p-2">
+        <div className="rounded bg-bg22 p-8">
+          <Link to="/projetos">
+            {<img
+              alt="Logo da Beelieve"
+              src={Beelieve}
+              className="cursor-pointer"
+              width="180px"
+            />}
+          </Link>
+        </div>
+        <div className="flex h-full flex-col rounded bg-bg22 px-5">
+          <div className="h-full">
+            <ul className="my-10 flex flex-col gap-3">
+              <li className="flex cursor-pointer flex-row items-center gap-1 p-1.5 text-xl font-medium text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22">
+                <PiProjectorScreenChart
+                  color="#DADDE6"
+                  size={24}
+                  className="hover:fill-primary50"
+                />
+                <Link to="/projetos">Projetos</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="mb-5 flex gap-2">
+            <div className="flex h-[50px] flex-col">
+              <p className="text-base font-semibold text-on-bg22">
+                {data.nomeUsuario}
+              </p>
+              <p className="font-regular text-sm text-on-bg22">Líder de Pacote de Trabalho</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex cursor-pointer items-center gap-2 rounded bg-bg22 p-3 text-lg font-semibold text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22">
+          <BiLogOut color="#DADDE6" size={24} />
+          <p>Sair</p>
+        </div>
+      </aside>
+    )
+  }
+
+  if (cargoUsuario === 'Analista') {
+    return (
+      <aside className="sticky top-0 flex h-screen min-w-max flex-col gap-3 bg-bg15 p-2">
+        <div className="rounded bg-bg22 p-8">
+          <Link to="/projetos">
+            {<img
+              alt="Logo da Beelieve"
+              src={Beelieve}
+              className="cursor-pointer"
+              width="180px"
+            />}
+          </Link>
+        </div>
+        <div className="flex h-full flex-col rounded bg-bg22 px-5">
+          <div className="h-full">
+            <ul className="my-10 flex flex-col gap-3">
+              <li className="flex cursor-pointer flex-row items-center gap-1 p-1.5 text-xl font-medium text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22">
+                <PiProjectorScreenChart
+                  color="#DADDE6"
+                  size={24}
+                  className="hover:fill-primary50"
+                />
+                <Link to="/projetos">Projetos</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="mb-5 flex gap-2">
+            <div className="flex h-[50px] flex-col">
+              <p className="text-base font-semibold text-on-bg22">
+                {data.nomeUsuario}
+              </p>
+              <p className="font-regular text-sm text-on-bg22">Analista</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex cursor-pointer items-center gap-2 rounded bg-bg22 p-3 text-lg font-semibold text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22">
+          <BiLogOut color="#DADDE6" size={24} />
+          <p>Sair</p>
+        </div>
+      </aside>
+    )
+  }
 }
 
 export default NavBar
