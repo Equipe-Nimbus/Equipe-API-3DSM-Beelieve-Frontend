ed48404075ae0a400d10d3d514593350fa9f7843
Vinicius <yinioliveira@gmail.com>
2023-11-21 00:27:35 -0300

    feat: complementei o form de login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,34 +1,31 @@
 import React, {useState} from "react"
+import { useForm } from "react-hook-form"
 import detalhe from '../images/detalhe.png'
 import Beelieve1 from '../images/Beelieve1.png'
 
+import axios from "../services/axios"
+
 function Login(){
-  const [credenciais, setCredenciais] = useState({
-    login: '',
-    senha: '',
-  });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setCredenciais({
-      ...credenciais,
-      [name]: value,
-    });
-  };
+  const { register, handleSubmit } = useForm({
+  })
 
-  const handleLogin = () => {
-    const credenciaisJSON = JSON.stringify(credenciais);
-    console.log(credenciaisJSON);
+  const handleLogin = async (data) => {
+    //console.log(data)
+
+    await axios.post("/usuario/login", data).then((response) => {
+      console.log(response)
+    })
   };
 
     return(
-        <div class="bg-gradient-to-t from-bg33 via-bg33 to-bg100 h-screen flex items-center justify-center">
+        <div className="bg-gradient-to-t from-bg33 via-bg33 to-bg100 h-screen flex items-center justify-center">
             <div className="shadow-xl bg-gradient-to-t from-bg98 via-bg98 to-bg100 w-4/5 h-4/5 p-8 rounded-md">
         <div>
         <img src={Beelieve1} alt="beelieveLogo" className="mb-5" />
         <img src={detalhe} alt="detalhe" className="h-auto" />
         </div>
-        <form className="max-w-md mx-auto mt-[-25rem] mr-[10rem]">
+        <form onSubmit={handleSubmit(handleLogin)} className="max-w-md mx-auto mt-[-25rem] mr-[10rem]">
         <h2>Bem-Vindo</h2>
         <br></br>
           <div className="mb-4">
@@ -41,8 +38,7 @@ function Login(){
               name="login"
               className="w-full border border-none p-2"
               required
-              value={credenciais.email}
-              onChange={handleInputChange}
+              {...register("login")}
             />
           </div>
 
@@ -56,8 +52,7 @@ function Login(){
               name="senha"
               className="w-full border border-none p-2"
               required
-              value={credenciais.senha}
-              onChange={handleInputChange}
+              {...register("senha")}
             />
           </div>
 
@@ -65,7 +60,6 @@ function Login(){
             type="submit"
             className="p-2 bg-primary50 rounded px-8 hover:bg-bg33"
             value="Entrar"
-            onClick={handleLogin}
           />
         </form>
         </div>
